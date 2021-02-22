b90afe80dbc90aed5a336b0cc3163b7a96311f96
kraimaim <67071210+kraimaim@users.noreply.github.com>
2021-02-22 11:22:17 -0300

    archivos subidos

---
+++ scripts/landingPage.js
@@ -0,0 +1,8 @@
+const selectorBoton = document.getElementById('botonSumate')
+
+
+const funcioncita = function () {
+    alert('Sumate al banco de Bokita, el mejor banco del mejor Club!')
+}
+
+selectorBoton.addEventListener('click', funcioncita)
